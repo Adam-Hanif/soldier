f425db5e21c27bde8eed293fd69302ce3ce0f201
Adam-IT <Alan_Salons@mail.ru>
2023-05-17 10:54:16 +0300

    создал модель солдата

---
+++ soldier.js
@@ -0,0 +1,12 @@
+const soldier = {
+  name: "Djon",
+  health: 10,
+  weapon: {
+    nameWeapon: "АК-47",
+    quantityСartridges: 30,
+  },
+  supplies: 3,
+  shoots: function () {},
+  reloads: function () {},
+  injured: function () {},
+};
