23f26720821bbd9c611b9a2b998ede50f075ea9e
Adam-IT <Alan_Salons@mail.ru>
2023-05-17 12:09:34 +0300

    доработfk методы объекта, чтобы солдат мог воевать.

--- soldier.js
+++ soldier.js
@@ -6,7 +6,26 @@ const soldier = {
     quantityСartridges: 30,
   },
   supplies: 3,
-  shoots: function () {},
-  reloads: function () {},
-  injured: function () {},
+  shoots: function () {
+    if (this.weapon.quantityСartridges > 0) {
+      this.weapon.quantityСartridges--;
+      return console.log("бах бах");
+    } else {
+      return console.log("обойма пуста. Перезаредитесь.");
+    }
+  },
+  reloads: function () {
+    this.weapon.quantityСartridges = 30;
+    this.supplie--;
+  },
+  injured: function () {
+    if (this.health > 0) {
+      this.health--;
+      return console.log(this.health);
+    } else {
+      return console.log("Ты проиграл.");
+    }
+  },
 };
+
+// console.log(soldier.health);
